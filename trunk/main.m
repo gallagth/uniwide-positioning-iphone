26f9a25b35d396837166ed5aca412a1f3be01008
gallagth@gmail.com <gallagth@gmail.com@e662aa30-fb01-b24c-00ea-f04f97df8766>
2011-03-21 23:14:04 +0000

    Added initial files

---
+++ trunk/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UNSW WiPos
+//
+//  Created by Sean Flynn on 22/9/10.
+//  Copyright University of New South Wales 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
